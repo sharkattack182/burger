64ad8656f98c2dfbdc25009797429e38abe99b2b
Matthew Courtney <matthewdc182@gmail.com>
2020-09-09 20:27:43 -0400

    added in the config controllers and the views folders

---
+++ config/orm.js
@@ -0,0 +1,8 @@
+var connection = require("../config/connection.js");
+
+selectAll()
+insertOne()
+updateOne()
+
+
+module.exports = orm;
---
+++ controllers/burgers_controller.js
@@ -0,0 +1,10 @@
+var express= require("express");
+var burger = require("../burgers.js");
+
+//get method
+
+//post method
+
+//put method
+
+module.exports = router;
